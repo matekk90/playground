085b1146450f2a24b368e171ebfe4f864649ebd9
matekk90 <matekk90@gmail.com>
2015-07-16 20:24:12 +0200

    Basic REST controller

---
+++ src/main/java/pl/krupop/m/playground/EntryPoint.java
@@ -0,0 +1,16 @@
+package pl.krupop.m.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(value="pl.krupop.m.controllers")
+public class EntryPoint 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        SpringApplication.run(EntryPoint.class, args);
+    }
+}
