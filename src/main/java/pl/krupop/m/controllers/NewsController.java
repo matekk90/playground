14f321edb792a79e5e26de0d508f45a902397e90
matekk90 <matekk90@gmail.com>
2015-09-23 20:02:54 +0200

    Add News Controller - saves News (without events) and logs ous events

---
+++ src/main/java/pl/krupop/m/controllers/NewsController.java
@@ -0,0 +1,35 @@
+package pl.krupop.m.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import pl.krupop.m.dao.NewsRepository;
+import pl.krupop.m.dto.NewsDto;
+import pl.krupop.m.entity.Event;
+import pl.krupop.m.entity.News;
+
+@RestController
+@RequestMapping(value = "/news")
+public class NewsController {
+
+    private static final Logger logger = LoggerFactory.getLogger(NewsController.class);
+
+    @Autowired
+    NewsRepository newsRepository;
+
+    @RequestMapping(method = RequestMethod.POST, consumes = "application/json")
+    public void addNews(@RequestBody NewsDto newsDto) {
+        logger.info("Saving news with title: {} and body: {} to database", newsDto.getTitle(), newsDto.getBody());
+        News news = new News(newsDto.getTitle(), newsDto.getBody());
+        newsRepository.save(news);
+        for (Event event : newsDto.getEvents()) {
+            logger.info("Saving event {} {} to database", event.getTitle(), event.getBody());
+        }
+    }
+
+}
