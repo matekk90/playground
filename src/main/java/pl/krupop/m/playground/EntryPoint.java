8d24df01086c51addb83e54d29671c52bc3bf145
matekk90 <matekk90@gmail.com>
2015-09-23 19:51:38 +0200

    Remowe "Hello world"

--- src/main/java/pl/krupop/m/playground/EntryPoint.java
+++ src/main/java/pl/krupop/m/playground/EntryPoint.java
@@ -12,7 +12,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories("pl.krupop.m.dao")
 public class EntryPoint {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         SpringApplication.run(EntryPoint.class, args);
     }
 }
