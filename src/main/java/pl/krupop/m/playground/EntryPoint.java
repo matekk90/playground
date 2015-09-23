37b28dc8f77121bce45d71f3962ebcc01c9f52b5
matekk90 <matekk90@gmail.com>
2015-09-23 19:49:37 +0200

    Add database

--- src/main/java/pl/krupop/m/playground/EntryPoint.java
+++ src/main/java/pl/krupop/m/playground/EntryPoint.java
@@ -2,10 +2,14 @@ package pl.krupop.m.playground;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(value = "pl.krupop.m.controllers")
+@EntityScan(value = "pl.krupop.m.*")
+@EnableJpaRepositories("pl.krupop.m.dao")
 public class EntryPoint {
     public static void main(String[] args) {
         System.out.println("Hello World!");
